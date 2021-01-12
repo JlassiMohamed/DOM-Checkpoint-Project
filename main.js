6a3165b21977a14b403dbafc375afe6129ab02d0
JlassiMohamed <72744617+JlassiMohamed@users.noreply.github.com>
2021-01-13 00:16:20 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,67 @@
 let plus = document.querySelectorAll(".plus");
 let minus = document.querySelectorAll(".minus");
-let qte = document.getElementsByClassName("qte");
-let price = document.getElementsByClassName("price");
-let subtotal = document.getElementsByClassName("subtotal")
-let item = document.querySelectorAll(".item")
-let cancel = document.getElementsByClassName("cancel");
-let card = document.getElementsByClassName("card")
+let qte = Array.from(document.getElementsByClassName("qte"));
+let cancel = Array.from(document.getElementsByClassName("cancel"));
+let card = Array.from(document.getElementsByClassName("card"));
+let like = Array.from(document.getElementsByClassName("fa-heart"));
+let price = Array.from(document.getElementsByClassName("price"));
+let subtotal = document.querySelector(".subtotal");
+let item = document.querySelector(".item");
+
+totalPrice();
+totalItems();
 
 for (let i = 0; i < plus.length; i++) {
-  let initialQte = Number(qte[0].innerHTML);
-  item[0].innerHTML = Number(item[0].innerHTML) + initialQte;
-  let unitPrice = Number(price[i].innerHTML);
-  subtotal[0].innerHTML = Number(subtotal[0].innerHTML) + unitPrice
-
   plus[i].addEventListener("click", function () {
-  qte[i].innerHTML = Number(qte[i].innerHTML) + 1;
-  price[i].innerHTML = unitPrice*Number(qte[i].innerHTML);
-  subtotal[0].innerHTML = Number(subtotal[0].innerHTML) + unitPrice;
-  item[0].innerHTML = Number(item[0].innerHTML) + initialQte;
-  });
+    qte[i].innerHTML = Number(qte[i].innerHTML) + 1;
+    totalPrice();
+    totalItems()
+  })
+}
   
+for (let i = 0; i < plus.length; i++) {
   minus[i].addEventListener("click", function () {
-  if(Number(qte[i].innerHTML)>1){
-  qte[i].innerHTML = Number(qte[i].innerHTML) - 1;
-  price[i].innerHTML = unitPrice*Number(qte[i].innerHTML);
-  subtotal[0].innerHTML = Number(subtotal[0].innerHTML) - unitPrice;
-      item[0].innerHTML = Number(item[0].innerHTML) - initialQte;
-  }});
+    if(Number(qte[i].innerHTML)>1){
+    qte[i].innerHTML = Number(qte[i].innerHTML) - 1;
+    totalPrice();
+    totalItems()
+    }
+  })
+}
 
+for (let i = 0; i < plus.length; i++) {
+  like[i].addEventListener("click", function () {
+    if(like[i].style.color==="black"){
+      like[i].style.color="#B91917"}
+    else{
+      like[i].style.color="black"
+    }
+  })
+}
+  
+for (let i = 0; i < plus.length; i++) { 
   cancel[i].addEventListener("click", function () {
-  card[i].remove();})
+    card[i].remove();
+    totalPrice();
+    totalItems()
+  })
+}
+
+function totalPrice(){
+  let qte = Array.from(document.getElementsByClassName("qte"));
+  let price = Array.from(document.getElementsByClassName("price"));
+  let s = 0;
+  for (let i = 0; i < price.length; i++) {
+    s = s + Number(qte[i].innerHTML)*Number(price[i].innerHTML);
+    subtotal.innerHTML = s
+  }
+}
+
+function totalItems(){
+  let qte = Array.from(document.getElementsByClassName("qte"));
+  let s = 0;
+  for (let i in qte){
+    s = s + Number(qte[i].innerHTML);
+  }
+  item.innerHTML = s
 }
